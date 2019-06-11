d6dfe79c6cca5e5e41cfe1d85c1058dac5a465fc
Alexandr <aleksandr.zanko@enway.com>
2019-06-11 17:18:36 +0300

    button

--- force-app/main/default/lwc/step2/step2.js
+++ force-app/main/default/lwc/step2/step2.js
@@ -1,5 +1,27 @@
+import { track } from 'lwc';
 import BaseStep from 'c/baseStep';
 
 export default class Step2 extends BaseStep {
 
+    @track ggf = null;
+    @track button;
+
+    renderedCallback() {
+        this.button = this.template.querySelector('.submit-js');
+    }
+
+    gg() {
+        console.log('button click');
+        if (this.button.disabled) {
+            this.button.disabled = null;
+        } else {
+            this.button.disabled = "disabled";
+        }
+
+        setTimeout(() => {
+            console.log('clear');
+            this.button.disabled = null;
+        }, 3000);
+
+    }
 }
